dd81b62b6ad9c654bdd7c9e01f9cf2308f6278f8
rayane <rayquezia03@gmail.com>
2024-03-05 22:36:16 -0300

    add mores endpoints services

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('scheduler/', views.agendar, name='agendar'),
     path('get_queue/', views.get_queue, name='get_queue'),
-    path('complete_service/', views.complete_service, name='complete_service'),
+    path('complete_current_customer_service/', views.complete_current_customer_service, name='complete_current_customer_service'),
+    path('complete_workday/', views.complete_workday, name='complete_workday'),
+    path('get_queue_completed/', views.get_queue_completed, name='get_queue_completed'),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -40,7 +40,10 @@ class Fila:
       valor = self.inicio.item
       self.inicio = self.inicio.proximo
       return valor, aux
-
+  
+  def resetar(self):
+    self.inicio = None
+    self.fim = None
 
   def imprimir(self):
     aux = self.inicio
@@ -60,7 +63,9 @@ def complete_current_customer_service(fila):
     return removed_client
 
 queue_service = Fila()
+all_clients_served = []
 
+#POST do client na fila
 def agendar(request):
     print(request.method)
     if request.method == 'POST':
@@ -73,25 +78,75 @@ def agendar(request):
             'name': nome,
             'servico': servico,
             'email': email,
-            'celular': celular
+            'celular': celular,
+            'served': False
         }
         
         add_client_to_queue(queue_service,data_client)
+        all_clients_served.append(data_client) 
+        
         client_name = data_client['name']
         client_services = data_client['servico']
    
-        print('$$$$$$$$$$$$$$$$444')
+        print('$$$$$$$$$$$$$$$$$$')
         print(queue_service.imprimir())
         
         return render(request, 'sucess.html', {'client_name': client_name,'client_services':client_services})
         
-        # return render(request, 'scheduler/resultado_agendamento.html', {'clientes': all_clients})
+        # return render(request, 'scheduler/resultado_agendamento.html', {'clientes': all_clients_served})
         return HttpResponse('sucesso!')
     else:
         return render(request, 'scheduler.html')
 
+#GET da fila de pessoas que estão em espera
 def get_queue(request):
-   return render(request, "get_queue.html")
+  if queue_service.EstaVazia() != True:
+    
+      print('!!!!!!!!!!!!!!!!!')
+      print(queue_service.EstaVazia())
+      
+      people_in_stand_by = queue_service.imprimir()
+      return render(request, "get_queue.html", {'queue': people_in_stand_by })
+  else:
+    return HttpResponse("Não há clientes na fila!")
 
-def complete_service(request):
-   return render(request, "complete_service.html")
+#DELETE - concluir atendimento/remover cliente da fila
+def complete_current_customer_service(request):
+    if queue_service.EstaVazia() != True:
+      queue = queue_service.imprimir()
+      queue[0]['served'] = True
+      current_client = queue[0]['name']
+      
+      print('@@@@@@@@@@')
+      print(queue)
+      print(current_client)
+      
+      queue_service.remover()
+      return render(request, "complete_current_customer_service.html",{'current_client': current_client})
+    else:
+      return HttpResponse("Não há clientes na fila!")
+  
+#fornecer clientes que ja foram atendidas - "served" == True
+def get_queue_completed(request):
+  total_workday_clients = 0
+  finished_clients = []
+  if all_clients_served != None:
+    for client in all_clients_served:
+      print('***********')
+      print(client)
+      if client['served'] == True:
+        finished_clients.append(client)
+        total_workday_clients = total_workday_clients + 1
+      
+      print('------')
+      print(finished_clients)
+        
+  return render(request, "get_queue_completed.html",{'finished_clients': finished_clients, 'total_workday_clients':total_workday_clients})
+    
+#reseta a fila de atendimento
+def complete_workday(request):
+  all_clients_served = []
+  queue_service.resetar()
+  
+  return render(request, "complete_workday.html")
+  
